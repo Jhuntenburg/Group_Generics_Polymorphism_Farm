97cb4062b393d9bf07601c3a937e92384de6a97f
Minsoo Jo <134647176+Minsoo5@users.noreply.github.com>
2023-07-28 19:59:23 -0400

    Started Tractor

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/Vehicle/Tractor.java
@@ -0,0 +1,12 @@
+package com.zipcodewilmington.froilansfarm.Vehicle;
+
+public class Tractor extends Vehicle{
+
+
+    public Tractor(Person operator) {
+        super(operator);
+    }
+
+    public Vegetable harvest(Crop cropRow) {
+    }
+}
