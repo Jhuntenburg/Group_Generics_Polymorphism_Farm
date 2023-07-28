0f1fdeea10d86af22f2fb7eeb964687bbe5be84f
surabhi <surabhi.das@outlook.com>
2023-07-28 19:56:23 -0400

    kale tests

---
+++ src/test/java/com/zipcodewilmington/froilansfarm/KaleTest.java
@@ -0,0 +1,33 @@
+package com.zipcodewilmington.froilansfarm;
+
+import org.junit.Assert;
+
+public class KaleTest {
+
+    public void kaleGotEatenTest(){
+        //Given
+        Kale kale = new Kale();
+        Froilan f= new Froilan();
+
+        //When people eat (under Eater)
+        f.eat(kale);
+
+        Assert.assertTrue(kale.gotEaten());
+
+    }
+
+    public void tomSetEatenStat(){
+        //we make egg its false that its not eata bc just born
+        Kale kale = new Kale();
+
+        //when setting true, checking if its been eaten
+        kale.setEatenStatus();
+
+        Assert.assertTrue(kale.gotEaten);
+
+    }
+
+
+
+
+}
