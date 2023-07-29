bb716707ef05bb7ed091429df868f31e26c64350
Joe <Jhuntenburg@gmail.com>
2023-07-29 16:07:47 -0400

    person classes added

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/Persons/Froilanda.java
@@ -0,0 +1,47 @@
+package com.zipcodewilmington.froilansfarm.Persons;
+
+import com.zipcodewilmington.froilansfarm.StorageUnits.Pantry;
+import com.zipcodewilmington.froilansfarm.Vehicle.CropDuster;
+
+public class Froilanda extends Person implements Rider, Pilot{
+    int eatenfood=0;
+
+    public Froilanda(String name) {
+        super(name);
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println("Look out below!");
+
+    }
+
+    @Override
+    public void eat(Edible edible) {
+        Pantry.remove(edible);
+        eatenfood ++;
+
+    }
+
+    @Override
+    public void feed(Animal animal, Edible edible) {
+        animal.eat(edible);
+
+    }
+
+    @Override
+    public void flys(CropDuster cropDuster) {
+        cropDuster.fly();
+
+    }
+
+    @Override
+    public void mount(Rideable rideable) {
+        rideable.getMounted(this);
+    }
+
+    @Override
+    public void dismount(Rideable rideable) {
+        rideable.getMounted(null);
+    }
+}
