7b873e931d11ac3f0277798e2a2bb279b668d130
Joe <Jhuntenburg@gmail.com>
2023-07-29 14:25:54 -0400

    Person update

--- src/main/java/Persons/Person.java
+++ src/main/java/Persons/Person.java
@@ -1,6 +1,6 @@
 package Persons;
 
-public abstract class Person extends Animal {
+public abstract class Person extends Animal implements Feeder {
     public Person(String name) {
         super(name);
     }
