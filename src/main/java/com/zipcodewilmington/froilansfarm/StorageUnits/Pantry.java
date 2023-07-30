f6bd0230729939859fbc198d312febe61144f645
Brent <BrentC0328@gmail.com>
2023-07-30 15:20:16 -0400

    Update pantry

--- src/main/java/com/zipcodewilmington/froilansfarm/StorageUnits/Pantry.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/StorageUnits/Pantry.java
@@ -1,10 +1,11 @@
 package com.zipcodewilmington.froilansfarm.StorageUnits;
 
-import Edible.Edible;
+
+import Edible.*;
 
 import java.util.ArrayList;
 
-public class Pantry extends StorageUnit<Edible>{
+public class Pantry extends ArrayList<Edible>{
 
     private static ArrayList<Tomato> tomatoList;
     private static ArrayList<Kale> kaleList;
