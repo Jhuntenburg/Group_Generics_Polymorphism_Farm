ac25b46f20599ec8e47fd5a4bb34e96824dc9abc
Brent <BrentC0328@gmail.com>
2023-07-30 15:26:50 -0400

    pushing pantry constructor

--- src/main/java/com/zipcodewilmington/froilansfarm/StorageUnits/Pantry.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/StorageUnits/Pantry.java
@@ -12,5 +12,13 @@ public class Pantry extends ArrayList<Edible>{
     private static ArrayList<EarCorn> earCornList;
     private static ArrayList<Egg> eggList;
 
+    public Pantry() {
+        this.tomatoList = new ArrayList<Tomato>();
+        this.kaleList = new ArrayList<Kale>();
+        this.earCornList = new ArrayList<EarCorn>();
+        this.eggList = new ArrayList<Egg>();
+    }
+
+
 
 }
