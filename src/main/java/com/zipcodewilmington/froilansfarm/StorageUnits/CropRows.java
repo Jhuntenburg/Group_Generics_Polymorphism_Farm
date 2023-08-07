279dd8cfae3ca23d92ddfc5c54118107af664ca1
Minsoo Jo <134647176+Minsoo5@users.noreply.github.com>
2023-08-07 16:50:57 -0400

    conflict resolved

--- src/main/java/com/zipcodewilmington/froilansfarm/StorageUnits/CropRows.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/StorageUnits/CropRows.java
@@ -2,14 +2,11 @@ package com.zipcodewilmington.froilansfarm.StorageUnits;
 
 import com.zipcodewilmington.froilansfarm.Crop.Crop;
 
-<<<<<<< HEAD
-public class CropRows<AnyType extends Crop> extends StorageUnit<Crop>{
-=======
+
 public class CropRows <Anything extends Crop> extends StorageUnit<Anything> {
 
     public CropRows() {
         super();
     }
->>>>>>> min
 
 }
