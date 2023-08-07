78a3a1d94926103d92b70c587abfe09b9e317d14
Minsoo Jo <134647176+Minsoo5@users.noreply.github.com>
2023-08-07 15:31:13 -0400

    crops

--- src/main/java/com/zipcodewilmington/froilansfarm/StorageUnits/CropRows.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/StorageUnits/CropRows.java
@@ -2,6 +2,6 @@ package com.zipcodewilmington.froilansfarm.StorageUnits;
 
 import com.zipcodewilmington.froilansfarm.Crop.Crop;
 
-public class CropRows<AnyType extends Crop> extends StorageUnit{
+public class CropRows<AnyType extends Crop> extends StorageUnit<Crop>{
 
 }
